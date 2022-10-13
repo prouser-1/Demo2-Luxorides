1619ee7052b5ac3e6a588084ed13316717cbfa04
prouser-1 <111452794+prouser-1@users.noreply.github.com>
2022-10-13 12:48:50 +0300

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,34 @@
+document.getElementById("main-action").onclick = function () {
+    document.getElementById("cars").scrollIntoView({behavior: "snooth"});
+}
 
+var buttons = document.getElementsByClassName("car-button");
+for (var i = 0; i < buttons.length; i++) {
+    buttons[i].onclick = function () {
+        document.getElementById("price").scrollIntoView({behavior: "snooth"});
+    }
+}
+
+document.getElementById("price-action").onclick = function () {
+    if (document.getElementById("name").value === "") {
+        alert("Заполните поле имя");
+    } else if (document.getElementById("phone").value === "") {
+        alert("Заполните поле телефон");
+    } else if (document.getElementById("car").value === "") {
+        alert("Заполните поле автомобиль");
+    } else {
+        alert("Спасибо за заявку, мы свами свяжемся в ближайшее время");
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    let layer = document.querySelector('.price-image');
+    document.addEventListener('mousemove', (event) => {
+        layer.style.transform = 'translate3d(' + ((event.clientX * 0.2) / 8) + 'px,' + ((event.clientY * 0.2) / 8) + 'px,0px)';
+    });
+
+    const elem = document.querySelector(".main");
+    document.addEventListener('scroll', () => {
+        elem.style.backgroundPositionX = '0' + (0.3 * window.pageYOffset) + 'px';
+    })
+});
